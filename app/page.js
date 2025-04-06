d0cbde6b1cb41e2a9c11f43dc357a63092cae62b
QHuyyyyy <huy120904@gmail.com>
2025-04-06 22:09:24 +0700

    add and config theme and layer tailwind in global.css

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main>
-    <h1>HomePage</h1>
-   </main>
+    <main>
+      <h1>homepage</h1>
+    </main>
   );
 }
