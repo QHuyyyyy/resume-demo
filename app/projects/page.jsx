4e61468f08f796f050390673b19eada992e18986
QHuyyyyy <huy120904@gmail.com>
2025-04-08 13:19:20 +0700

    add hover projects page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,4 +1,3 @@
-
 "use client"
 import { motion } from "framer-motion"
 import { Swiper, SwiperSlide } from "swiper/react"
@@ -14,35 +13,43 @@ const projects = [
     {
         num: "01",
         category: "Web Development",
-        title: "Project 1",
-        description: "Lorem ipsum dolor sit amet consectetur \
-        adipisicing elit.Quisquam, voluptatibus.",
+        title: "Claim Request",
+        description: " Website manage project\
+ Handle request approvals and claims\
+ Manage user roles and permissions.",
         stack: [
-            "React", "css   ", "tailwind", "nextjs", "typescript"
+            "React", "Typescript", "Tailwindcss","AntDesign"
         ],
-        image: "/assets/work/thumb1.png",
-        live: "",
-        github: ""
+        image: "/assets/work/thumb5.png",
+        live: "https://group-final-project.vercel.app",
+        github: "https://github.com/QHuyyyyy/Group_Final_Project"
     },
     {
         num: "02",
         category: "Web Development",
-        title: "Project 2",
+        title: "Clone Layout",
         description: "Lorem ipsum dolor sit amet consectetur \
         adipisicing elit.Quisquam, voluptatibus.",
         stack: [
-            "React", "css   ", "tailwind", "nextjs", "typescript"
+            "HTML", "CSS",
         ],
-        image: "/assets/work/thumb2.png",
-        live: "",
-        github: ""
+        image: "/assets/work/thumb6.png",
+        live: "https://bgin-qhuys-projects.vercel.app/",
+        github: "https://github.com/QHuyyyyy/Bgin"
     },
 ]
 export default function Project() {
     const [project, setProject] = useState(projects[0]);
+    const [hoverState, setHoverState] = useState({
+        number: false,
+        live: false,
+        github: false
+    });
+
     const handleSlideChange = (swiper) => {
         setProject(projects[swiper.activeIndex]);
     };
+
     return (
         <motion.section
             initial={{ opacity: 0 }}
@@ -53,17 +60,28 @@ export default function Project() {
             className="min-h-[80vh] flex flex-col justify-center xl:px-0 py-12"
         >
             <div className="container">
-                <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-                    <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col
-                    xl:justify-between order-2 xl:order-none">
-                        <div className="flex flex-col gap-[30px] h-[50%]">
-                            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.num}</div>
+                <div className="flex flex-col xl:flex-row xl:gap-[20px]">
+                    <div className="w-full xl:w-[40%] xl:h-[460px] flex flex-col
+                    xl:justify-between order-2 xl:order-none group">
+                        <div className="flex flex-col gap-[25px] h-[50%]">
+                            <div 
+                                className={`text-8xl leading-none font-extrabold text-transparent ${
+                                    hoverState.number || hoverState.live || hoverState.github 
+                                    ? "[-webkit-text-stroke:1px_#00ff99]" 
+                                    : "text-outline"
+                                }`}
+                                onMouseEnter={() => setHoverState({...hoverState, number: true})}
+                                onMouseLeave={() => setHoverState({...hoverState, number: false})}
+                            >
+                                {project.num}
+                            </div>
 
-                            <h2 className="text-[42px] font-bold leading-none text-white transition-all duration-500">{project.category} project</h2>
-                            <p className="text-white/60">{project.description}</p>
-                            <ul className="flex gap-4 ">
+                            <h2 className="text-[42px] font-bold leading-none text-white transition-all duration-500">{project.title}</h2>
+                            
+                            <p className="text-white/60"> <p className="text-white/60 text-[150%]">{project.category}</p>{project.description}</p>
+                            <ul className="flex gap-4 flex-wrap gap-2 ">
                                 {project.stack.map((item, index) => (
-                                    <li key={index} className="text-accent text-2xl">
+                                    <li key={index} className="text-accent text-xl ">
                                         {item}
                                         {index !== project.stack.length - 1 && <span className="text-white/60">,</span>}
                                     </li>
@@ -71,34 +89,42 @@ export default function Project() {
                             </ul>
                             <div className="border boder-white/20"></div>
                             <div className="flex items-center gap-4">
-                                <Link href={project.live}>
+                                <a href={project.live} target="_blank">
                                     <TooltipProvider>
                                         <Tooltip>
-                                            <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/50 flex justify-center items-center group">
-                                                <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                                            <TooltipTrigger 
+                                                className="w-[70px] h-[70px] rounded-full bg-white/50 flex justify-center items-center"
+                                                onMouseEnter={() => setHoverState({...hoverState, live: true})}
+                                                onMouseLeave={() => setHoverState({...hoverState, live: false})}
+                                            >
+                                                <BsArrowUpRight className={`text-3xl ${hoverState.live ? "text-accent" : "text-white"}`} />
                                             </TooltipTrigger>
                                             <TooltipContent>
                                                 <p>Live project</p>
                                             </TooltipContent>
                                         </Tooltip>
                                     </TooltipProvider>
-                                </Link>
-                                <Link href={project.github}>
+                                </a>
+                                <a href={project.github} target="_blank">
                                     <TooltipProvider>
                                         <Tooltip>
-                                            <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/50 flex justify-center items-center group">
-                                                <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                                            <TooltipTrigger 
+                                                className="w-[70px] h-[70px] rounded-full bg-white/50 flex justify-center items-center"
+                                                onMouseEnter={() => setHoverState({...hoverState, github: true})}
+                                                onMouseLeave={() => setHoverState({...hoverState, github: false})}
+                                            >
+                                                <BsGithub className={`text-3xl ${hoverState.github ? "text-accent" : "text-white"}`} />
                                             </TooltipTrigger>
                                             <TooltipContent>
-                                                <p>Github Respository</p>
+                                                <p>Github Repository</p>
                                             </TooltipContent>
                                         </Tooltip>
                                     </TooltipProvider>
-                                </Link>
+                                </a>
                             </div>
                         </div>
                     </div>
-                    <div className="w-full xl:w-[50%]">
+                    <div className="w-full xl:w-[60%]">
                         <Swiper
                             onSlideChange={handleSlideChange}
                             spaceBetween={30} slidesPerView={1} className="w-full xl:h-[520px] mb-12">
